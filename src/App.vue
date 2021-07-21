d2c6334f885d4d984faca25c39ad80bf0cddcc1e
SANGUK <jsw9330@icloud.com>
2021-07-21 23:03:20 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <hr />
+  <h1>카운트 {{ count }}</h1>
+  <button @click="btnClick"></button>
+  <Test />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Test from './components/test1.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return{
+      count: 0,
+    }
+  },
+  components: {
+    HelloWorld,
+    Test
+  },
+  methods: {
+    btnClick() {
+      this.count += 1;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/test1.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>안녕하세요. 첫 번째 컴포넌트 입니다.</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    countUp() {
+      this.count += 1;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
